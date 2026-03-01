46edd5e1e45d0f2a63450e8faec5a82e4c3bed02
Tabesh <tabeshhoseini85@gmail.com>
2026-03-01 11:17:32 +0330

    add first menu print in ui

--- src/UI.java
+++ src/UI.java
@@ -4,12 +4,21 @@ public class UI {
     static Scanner inputReader = new Scanner(System.in);
 
     public static void runGame() {
-        Board board = new Board(0, 4);
-        for (int i = 0; i < board.getBoardLength(); i++) {
-            for (int j = 0; j < board.getBoardLength(); j++) {
-                System.out.print(board.getBoard()[i][j]);
-            }
-            System.out.println();
-        }
+        showLoginMenu();
+        // Board board = new Board(0, 4);
+        // for (int i = 0; i < board.getBoardLength(); i++) {
+        // for (int j = 0; j < board.getBoardLength(); j++) {
+        // System.out.print(board.getBoard()[i][j]);
+        // }
+        // System.out.println();
     }
+
+    private static void showLoginMenu() {
+        System.out.println("___________2048___________\n" +
+                "1. Login\n" +
+                "2. Sign up\n" +
+                "3. League table\n" +
+                "0. Exit");
+    }
+
 }
